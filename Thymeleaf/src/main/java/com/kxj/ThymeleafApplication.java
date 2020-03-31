28adee57ba10df983c97f747d3cb968ac0b9d582
kong0827 <1351882069@qq.com>
2020-03-31 22:14:22 +0800

    修改依赖

---
+++ Thymeleaf/src/main/java/com/kxj/ThymeleafApplication.java
@@ -0,0 +1,16 @@
+package com.kxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kxj
+ * @date 2020/3/27 0:04
+ * @desc
+ */
+@SpringBootApplication
+public class ThymeleafApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafApplication.class, args);
+    }
+}
