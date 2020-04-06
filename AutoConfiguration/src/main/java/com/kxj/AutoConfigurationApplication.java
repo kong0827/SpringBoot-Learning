8c6adca2b7294f574bd450bb15acfeba71b3a964
kong0827 <1351882069@qq.com>
2020-04-06 23:41:06 +0800

    自动配置原理

---
+++ AutoConfiguration/src/main/java/com/kxj/AutoConfigurationApplication.java
@@ -0,0 +1,16 @@
+package com.kxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kxj
+ * @date 2020/4/6 22:29
+ * @desc
+ */
+@SpringBootApplication
+public class AutoConfigurationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfigurationApplication.class, args);
+    }
+}
