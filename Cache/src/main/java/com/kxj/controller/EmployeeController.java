c2d7ea8c504784cc997466132f5f7fda58bb6548
kong0827 <1351882069@qq.com>
2020-04-13 00:34:03 +0800

    annotation @CacheConfig @Caching

--- Cache/src/main/java/com/kxj/controller/EmployeeController.java
+++ Cache/src/main/java/com/kxj/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.kxj.entity.Employee;
 import com.kxj.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,15 +12,33 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/4/12 21:47
  * @desc
  */
-@RestController("employee")
+@RestController
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
-    public Employee getEmployee(Integer id) {
+    @GetMapping("employee/{id}")
+    public Employee getEmployee(@PathVariable("id") Integer id) {
         Employee employee = employeeService.getEmployee(id);
+        return employee;
+    }
+
+    @GetMapping("employee")
+    public Employee updateEmployee(Employee employee) {
+        Employee emp = employeeService.updateEmployee(employee);
+        return emp;
+    }
+
+    @GetMapping("employee/delete/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id) {
+        employeeService.deleteEmployee(id);
+        return "success";
+    }
+
+    @GetMapping("employee/lastName/{lastName}")
+    public Employee getEmployee(@PathVariable("lastName") String lastName) {
+        Employee employee = employeeService.getEmployee(lastName);
         return employee;
     }
 }
--- Cache/src/main/java/com/kxj/mapper/EmployeeMapper.java
+++ Cache/src/main/java/com/kxj/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.kxj.mapper;
 
 import com.kxj.entity.Employee;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -16,6 +17,12 @@ public interface EmployeeMapper {
     @Select("select * from employee where id = #{id}")
     public Employee getEmployee(Integer id);
 
-    @Update("update employee set lastName = {lastName}, email=#{email}, gender={gender}, where id={id}")
+    @Update("update employee set lastName = #{lastName}, email=#{email}, gender= #{gender}, d_id = #{dId} where id= #{id}")
     public void updateEmployee(Employee employee);
+
+    @Delete("delete from employee where  id= #{id}")
+    public void deleteEmployee(Integer id);
+
+    @Select("select * from employee where lastName = #{lastName}")
+    public Employee getEmployeeByLastName(String lastName);
 }
