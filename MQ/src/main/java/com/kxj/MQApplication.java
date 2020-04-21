42de20b60def0eb78a231d5199621eddd6133100
kong0827 <1351882069@qq.com>
2020-04-22 00:32:07 +0800

    create mq project

---
+++ MQ/src/main/java/com/kxj/MQApplication.java
@@ -0,0 +1,16 @@
+package com.kxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kxj
+ * @date 2020/4/21 23:36
+ * @desc
+ */
+@SpringBootApplication
+public class MQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MQApplication.class, args);
+    }
+}
