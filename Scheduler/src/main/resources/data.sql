03a50b25d945ef820903cbf2ebcf0adfe4abbe64
kongxiangjin <1351882069@qq.com>
2020-10-23 13:55:22 +0800

    init database

---
+++ Scheduler/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+insert into `spring_scheduled_cron`
+values (1, 'org.javamaster.b2c.scheduled.task.DynamicPrintTask', '*/5 * * * * ?', '定时任务描述', 1);
+insert into `spring_scheduled_cron`
+values (2, 'org.javamaster.b2c.scheduled.task.DynamicPrintTask1', '*/5 * * * * ?', '定时任务描述1', 1);
+insert into `spring_scheduled_cron`
+values (3, 'org.javamaster.b2c.scheduled.task.DynamicPrintTask2', '*/5 * * * * ?', '定时任务描述2', 1);
