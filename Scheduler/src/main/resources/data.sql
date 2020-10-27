81a7ac697eb6e7b658c1f23e1c9de9f3cc4a8015
kongxiangjin <1351882069@qq.com>
2020-10-27 19:56:07 +0800

    定时任务简介

--- Scheduler/src/main/resources/data.sql
+++ Scheduler/src/main/resources/data.sql
@@ -1,7 +1,3 @@
-
-insert into `spring_scheduled_cron`
-values (1, 'org.javamaster.b2c.scheduled.task.DynamicPrintTask', '*/5 * * * * ?', '定时任务描述', 1);
-insert into `spring_scheduled_cron`
-values (2, 'org.javamaster.b2c.scheduled.task.DynamicPrintTask1', '*/5 * * * * ?', '定时任务描述1', 1);
-insert into `spring_scheduled_cron`
-values (3, 'org.javamaster.b2c.scheduled.task.DynamicPrintTask2', '*/5 * * * * ?', '定时任务描述2', 1);
+INSERT INTO `spring_scheduled_cron` VALUES (1, ''com.kxj.task.DynamicPrintTask'', ''*/5 * * * * ?'', ''定时任务描述'', 1);
+INSERT INTO `spring_scheduled_cron` VALUES (2, ''com.kxj.task.DynamicPrintTask1'', ''*/5 * * * * ?'', ''定时任务描述1'', 1);
+INSERT INTO `spring_scheduled_cron` VALUES (3, ''com.kxj.task.DynamicPrintTask2'', ''*/5 * * * * ?'', ''定时任务描述2'', 1);
--- Scheduler/src/main/resources/schema.sql
+++ Scheduler/src/main/resources/schema.sql
@@ -1,17 +1,11 @@
-drop table if exists `spring_scheduled_cron`;
-create table `spring_scheduled_cron` (
-  `cron_id`         int primary key           auto_increment
-  comment '主键id',
-  `cron_key`        varchar(128) not null unique
-  comment '定时任务完整类名',
-  `cron_expression` varchar(20)  not null
-  comment 'cron表达式',
-  `task_explain`    varchar(50)  not null     default ''
-  comment '任务描述',
-  `status`          tinyint      not null     default 1
-  comment '状态,1:正常;2:停用',
-  unique index cron_key_unique_idx(`cron_key`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COMMENT = '定时任务表';
+DROP TABLE IF EXISTS `spring_scheduled_cron`;
+CREATE TABLE `spring_scheduled_cron`  (
+  `cron_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `cron_key` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '定时任务完整类名',
+  `cron_expression` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'cron表达式',
+  `task_explain` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '任务描述',
+  `status` tinyint(4) NOT NULL DEFAULT 1 COMMENT '状态,1:正常;2:停用',
+  PRIMARY KEY (`cron_id`) USING BTREE,
+  UNIQUE INDEX `cron_key`(`cron_key`) USING BTREE,
+  UNIQUE INDEX `cron_key_unique_idx`(`cron_key`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '定时任务表' ROW_FORMAT = Dynamic;
