708cc3d4d42102dbe980e07687e1096699bbccb4
kongxiangjin <1351882069@qq.com>
2021-01-25 19:44:24 +0800

    多数据源配置

---
+++ MultiDataSourceJPA/src/main/java/com/kxj/MultiDataSourceJpaApplication.java
@@ -0,0 +1,15 @@
+package com.kxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiangjin.kong
+ * @date 2021/1/25 16:36
+ */
+@SpringBootApplication
+public class MultiDataSourceJpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDataSourceJpaApplication.class, args);
+    }
+}
