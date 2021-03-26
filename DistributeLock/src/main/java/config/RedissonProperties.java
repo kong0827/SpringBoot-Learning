827f89dd361eb16a88b762df17f0f25a9cde623a
kongxiangjin <1351882069@qq.com>
2021-03-26 18:33:49 +0800

    redisson 分布式锁

---
+++ DistributeLock/src/main/java/config/RedissonProperties.java
@@ -0,0 +1,111 @@
+package config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author xiangjin.kong
+ * @date 2021/3/25 10:34
+ */
+@ConfigurationProperties(prefix = "redisson")
+public class RedissonProperties {
+
+    private int timeout = 3000;
+
+    private String address;
+
+    private String password;
+
+    private int connectionPoolSize = 64;
+
+    private int connectionMinimumIdleSize = 10;
+
+    private int slaveConnectionPoolSize = 250;
+
+    private int masterConnectionPoolSize = 250;
+
+    private String[] sentinelAddresses;
+
+    private String masterName;
+
+    private String[] nodeAddress;
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getConnectionPoolSize() {
+        return connectionPoolSize;
+    }
+
+    public void setConnectionPoolSize(int connectionPoolSize) {
+        this.connectionPoolSize = connectionPoolSize;
+    }
+
+    public int getConnectionMinimumIdleSize() {
+        return connectionMinimumIdleSize;
+    }
+
+    public void setConnectionMinimumIdleSize(int connectionMinimumIdleSize) {
+        this.connectionMinimumIdleSize = connectionMinimumIdleSize;
+    }
+
+    public int getSlaveConnectionPoolSize() {
+        return slaveConnectionPoolSize;
+    }
+
+    public void setSlaveConnectionPoolSize(int slaveConnectionPoolSize) {
+        this.slaveConnectionPoolSize = slaveConnectionPoolSize;
+    }
+
+    public int getMasterConnectionPoolSize() {
+        return masterConnectionPoolSize;
+    }
+
+    public void setMasterConnectionPoolSize(int masterConnectionPoolSize) {
+        this.masterConnectionPoolSize = masterConnectionPoolSize;
+    }
+
+    public String[] getSentinelAddresses() {
+        return sentinelAddresses;
+    }
+
+    public void setSentinelAddresses(String[] sentinelAddresses) {
+        this.sentinelAddresses = sentinelAddresses;
+    }
+
+    public String getMasterName() {
+        return masterName;
+    }
+
+    public void setMasterName(String masterName) {
+        this.masterName = masterName;
+    }
+
+    public String[] getNodeAddress() {
+        return nodeAddress;
+    }
+
+    public void setNodeAddress(String[] nodeAddress) {
+        this.nodeAddress = nodeAddress;
+    }
+}
