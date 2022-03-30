2aa585d2b77df412dcd2913704f0bf0eaea49730
kongxiangjin <1351882069@qq.com>
2022-03-30 13:27:24 +0800

    feat: async文档补充

---
+++ compensation-mechanism/src/main/java/com/kxj/CompensationApplication.java
@@ -0,0 +1,16 @@
+package com.kxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiangjin.kong
+ * @date 2022/3/23 11:45
+ */
+@SpringBootApplication
+public class CompensationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompensationApplication.class, args);
+    }
+}
